3cec3e172b87e928ac1d37e009d20629dd500178
Jessica Azzopardi <vegan4life@live.co.uk>
2026-02-15 18:31:15 +0000

    last html and css changes for the day

--- lets-get-organised/src/js/script.js
+++ lets-get-organised/src/js/script.js
@@ -1,11 +1,1 @@
-function reloadFrame() {
-  const frame = document.querySelector("iframe");
-  frame.src = frame.src;
-}
 
-function toggleFullscreen() {
-  const frame = document.querySelector("iframe");
-  if (frame.requestFullscreen) {
-    frame.requestFullscreen();
-  }
-}
