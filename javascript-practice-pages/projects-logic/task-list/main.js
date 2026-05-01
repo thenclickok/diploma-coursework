2f2e01c022bf6ed239be6ff624d28ad410e50319
Jessica Azzopardi <vegan4life@live.co.uk>
2026-05-01 14:22:34 +0100

    completed task list javascript challenge

---
+++ javascript-practice-pages/projects-logic/task-list/main.js
@@ -0,0 +1,35 @@
+const listContainer = document.getElementById("list-container");
+const addButton = document.getElementById("add-list-item");
+const firstX = document.getElementById("first-x");
+const firstListItem = document.getElementById("first-list-item");
+
+addButton.addEventListener("click", function addElement() {
+  const existingItem = document.querySelector(".list-item");
+
+  const newItem = document.createElement("li");
+  newItem.classList.add("list-item");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  const text = document.createElement("input");
+  text.type = "text";
+  text.maxLength = 30;
+  text.classList.add("text-box");
+
+  const deleteButton = document.createElement("div");
+  deleteButton.classList.add("delete-box");
+  deleteButton.innerHTML = "X";
+  deleteButton.addEventListener("click", function deleteElement() {
+    newItem.remove();
+  });
+
+  newItem.appendChild(checkbox);
+  newItem.appendChild(text);
+  newItem.appendChild(deleteButton);
+  listContainer.appendChild(newItem);
+});
+
+firstX.addEventListener("click", () => {
+  firstListItem.remove();
+});
