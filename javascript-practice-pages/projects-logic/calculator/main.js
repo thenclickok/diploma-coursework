ca372fd42c9fe04c88ab1338cc0fada5f0ab76ad
Jessica Azzopardi <vegan4life@live.co.uk>
2026-05-20 18:49:15 +0100

    finished the calculator challenge including css

--- javascript-practice-pages/projects-logic/calculator/main.js
+++ javascript-practice-pages/projects-logic/calculator/main.js
@@ -1,7 +1,4 @@
-document.getElementById("add").addEventListener("click", calculate("add"));
-document.getElementById("subtract").addEventListener("click", calculate);
-document.getElementById("multiply").addEventListener("click", calculate);
-document.getElementById("divide").addEventListener("click", calculate);
+const clear = document.getElementById("clear");
 
 function calculate(operation) {
   const number1 = parseFloat(document.getElementById("number1").value);
@@ -19,7 +16,7 @@ function calculate(operation) {
         result = `${number1} - ${number2} = ${number1 - number2}`;
         break;
       case "multiply":
-        result = `${number1} * ${number2} = ${number1 * number2}`;
+        result = `${number1} x ${number2} = ${number1 * number2}`;
         break;
       case "divide":
         if (number2 === 0) {
@@ -35,3 +32,21 @@ function calculate(operation) {
 
   document.getElementById("result").innerText = result;
 }
+
+document
+  .getElementById("add")
+  .addEventListener("click", () => calculate("add"));
+document
+  .getElementById("subtract")
+  .addEventListener("click", () => calculate("subtract"));
+document
+  .getElementById("multiply")
+  .addEventListener("click", () => calculate("multiply"));
+document
+  .getElementById("divide")
+  .addEventListener("click", () => calculate("divide"));
+clear.addEventListener("click", () => {
+  document.getElementById("result").innerText = "Result";
+  document.getElementById("number1").value = "";
+  document.getElementById("number2").value = "";
+});
