8baf6a227186d2e99825d9338f78fac026f7a43d
Jessica Azzopardi <vegan4life@live.co.uk>
2026-05-26 20:15:25 +0100

    did css for the bank-app in js project

--- javascript-practice-pages/projects-logic/bank-app/main.js
+++ javascript-practice-pages/projects-logic/bank-app/main.js
@@ -42,6 +42,3 @@ withdrawalSubmit.addEventListener("click", () => {
     document.getElementById("message").textContent = `Withdrew: £${amount}`;
   }
 });
-
-//
-document.querySelector("h5").style.color = "red";
