94f8fad6d3bcdaa38f2b577a229498f3b6193c29
Jessica Azzopardi <vegan4life@live.co.uk>
2026-09-21 13:48:26 +0100

    added a hidden details list to the tickets in status board which show a list of job names when user clicks the ticket

--- job-board-app/src/components/StatusBoard.js
+++ job-board-app/src/components/StatusBoard.js
@@ -4,17 +4,34 @@ import inProgressImage from "../images/in-progress.svg";
 import failedImage from "../images/failed.svg";
 
 const StatusBoard = ({ jobs = [] }) => {
-  const completedCount = jobs.filter(
-    (job) => job.status === "Completed",
-  ).length;
-  const inProgressCount = jobs.filter((job) => job.status === "Running").length;
-  const failedCount = jobs.filter((job) => job.status === "Failed").length;
+  //filter actual list of jobs for each of the three statuses
+  const completedJobs = jobs.filter((job) => job.status === "Completed");
+  const inProgressJobs = jobs.filter((job) => job.status === "Running");
+  const failedJobs = jobs.filter((job) => job.status === "Failed");
+
+  //render the details list (hidden list in each ticket info commponent)
+  const renderDetails = (jobList) => {
+    if (jobList.length === 0) {
+      return <p>No ticket details</p>;
+    }
+    return (
+      <ul>
+        {jobList.map((job) => (
+          <li key={job.id}>
+            <strong>ID #{job.id}:</strong> "<i>{job.name}</i>"
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div className="status-board">
       <TicketInfo
         result="completed"
         image={completedImage}
-        count={completedCount}
+        count={completedJobs.length}
+        details={renderDetails(completedJobs)}
       >
         <p>Tickets Completed:</p>
       </TicketInfo>
@@ -22,12 +39,18 @@ const StatusBoard = ({ jobs = [] }) => {
       <TicketInfo
         result="in-progress"
         image={inProgressImage}
-        count={inProgressCount}
+        count={inProgressJobs.length}
+        details={renderDetails(inProgressJobs)}
       >
         <p>Tickets in Progress:</p>
       </TicketInfo>
 
-      <TicketInfo result="failed" image={failedImage} count={failedCount}>
+      <TicketInfo
+        result="failed"
+        image={failedImage}
+        count={failedJobs.length}
+        details={renderDetails(failedJobs)}
+      >
         <p>
           Tickets <br /> Failed:
         </p>
--- job-board-app/src/components/TicketInfo.js
+++ job-board-app/src/components/TicketInfo.js
@@ -1,9 +1,20 @@
-const TicketInfo = ({ result, image, count, children }) => {
+import { useState } from "react";
+
+const TicketInfo = ({ result, image, count, children, details }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <div className={`ticket-info ${result}`}>
+    /*showDetails is false so "ticket-details" div doesn't 
+    show until user clicks a div in the status board like 
+    "completed" to make showDetails true*/
+    <div
+      className={`ticket-info ${result}`}
+      onClick={() => setShowDetails(!showDetails)}
+    >
       <img src={image} alt={result} width="50" height="50" />
       {children}
       <p className="ticket-count">{count}</p>
+      {showDetails && <div className="ticket-details">{details}</div>}
     </div>
   );
 };
