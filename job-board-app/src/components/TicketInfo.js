ccdddedcc6daa58e34b1f234476dec57a2e8ee7a
Jessica Azzopardi <vegan4life@live.co.uk>
2026-09-22 12:16:27 +0100

    added useRef() to stop the animation running on page load or refresh, to only run when count value is changed

--- job-board-app/src/components/TicketInfo.js
+++ job-board-app/src/components/TicketInfo.js
@@ -1,19 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const TicketInfo = ({ result, image, count, children, details }) => {
   const [showDetails, setShowDetails] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
+  /*useRef here is to prevent animation running on page load
+  as I just want animation happening when the count value changes*/
+  const prevCountRef = useRef(); // useRef = undefined
 
   useEffect(() => {
-    //trigger animation class
-    setIsAnimating(true);
+    //skip animation on first page load
+    if (prevCountRef.current === undefined) {
+      prevCountRef.current = count; //after page load, assign this variable to count
+      return;
+    }
 
-    //remove the class after animation runs for 500 milliseconds
-    const timer = setTimeout(() => {
-      setIsAnimating(false);
-    }, 500);
+    //if count changes value, trigger animation class
+    if (prevCountRef.current !== count) {
+      setIsAnimating(true);
+      prevCountRef.current = count; //update the count after animation runs
 
-    return () => clearTimeout(timer);
+      //remove the status-pop class after animation runs for 500 milliseconds
+      const timer = setTimeout(() => {
+        setIsAnimating(false);
+      }, 500);
+
+      return () => clearTimeout(timer);
+    }
   }, [count]); /*the count is the second parameter 
     so useEffect/animation runs for any change to count*/
 
